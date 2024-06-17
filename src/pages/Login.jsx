a1af635fc4c69c7ca9a803439e005f00f47b6d2a
azamovbehruz <azamovbehruz@gmail.com>
2024-06-17 16:21:35 +0500

    Ready

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,55 +1,68 @@
-import { Form, Link, useActionData } from "react-router-dom";
+import { Form, Link } from "react-router-dom";
 import { useLogin } from "../hooks/useLogin";
-import { useEffect } from "react";
 import { useRegister } from "../hooks/useRegister";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-export const action = async ({ request }) => {
-  let formData = await request.formData();
-  let email = formData.get("email");
-  let password = formData.get("password");
-  return { email, password };
-};
 function Login() {
-  const data = useActionData();
   const { signin } = useLogin();
   const { registerWithGoogle } = useRegister();
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
   useEffect(() => {
-    if (data) {
-      signin(data);
-    }
-  }, [data]);
+    // Optionally add any additional logic here on component mount
+  }, []);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    signin(formData);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="h-screen grid place-content-center">
       <Form
-        method="post"
+        onSubmit={handleFormSubmit}
         className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
       >
         <h4 className="text-center font-bold text-3xl">Login</h4>
         <input
           className="input input-bordered w-full max-w-xs"
           type="email"
-          label="Email"
           name="email"
+          value={formData.email}
+          onChange={handleInputChange}
           placeholder="yourgmail@gmail.com"
         />
         <input
           className="input input-bordered w-full max-w-xs"
           type="password"
-          label="Password"
           name="password"
+          value={formData.password}
+          onChange={handleInputChange}
           placeholder="00000000"
         />
-        <button className=" mt-4 btn btn-primary btn-block capitalize">
+        <button
+          type="submit"
+          className="mt-4 btn btn-primary btn-block capitalize"
+        >
           Login
         </button>
         <button
           onClick={registerWithGoogle}
-          className="btn btn-primary w-full  mb-5"
+          className="btn btn-primary w-full mb-5"
         >
           <span className="text-2xl">Google</span>
         </button>
         <p className="text-center text-xl">
-          Not a member yet ?
+          Not a member yet?{" "}
           <Link to="/register" className="capitalize text-xl link">
             Registration
           </Link>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,70 +1,84 @@
-import { Link, Form, useActionData } from "react-router-dom";
+import { Link, Form } from "react-router-dom";
 import { useRegister } from "../hooks/useRegister";
-import { useEffect } from "react";
-
-export const action = async ({ request }) => {
-  let formData = await request.formData();
-  let displayName = formData.get("displayName");
-  let photoURL = formData.get("photoURL");
-  let email = formData.get("email");
-  let password = formData.get("password");
-  return { displayName, photoURL, email, password };
-};
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 function Register() {
-  const data = useActionData();
   const { registerWithGoogle, register } = useRegister();
-  useEffect(() => {
-    if (data) {
-      register(data);
-    }
-  }, [data]);
+  const [formData, setFormData] = useState({
+    displayName: "",
+    photoURL: "",
+    email: "",
+    password: "",
+  });
+
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    register(formData);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="h-screen grid place-content-center">
       <Form
-        method="post"
+        onSubmit={handleFormSubmit}
         className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
       >
         <h4 className="text-center font-bold text-3xl">Register</h4>
         <input
           className="input input-bordered w-full max-w-xs"
           type="text"
-          label="Display Name :"
           name="displayName"
+          value={formData.displayName}
+          onChange={handleInputChange}
           placeholder="User Name"
         />
         <input
           className="input input-bordered w-full max-w-xs"
           type="url"
-          label="Photo URL"
           name="photoURL"
+          value={formData.photoURL}
+          onChange={handleInputChange}
           placeholder="https://photoURL.com"
         />
         <input
           className="input input-bordered w-full max-w-xs"
           type="email"
-          label="E-mail"
           name="email"
+          value={formData.email}
+          onChange={handleInputChange}
           placeholder="test@gmail.com"
         />
         <input
           className="input input-bordered w-full max-w-xs"
           type="password"
-          label="Password"
           name="password"
+          value={formData.password}
+          onChange={handleInputChange}
           placeholder="00000000"
         />
-        <button className="mt-4 btn btn-primary btn-block capitalize">
-          Login
+        <button
+          type="submit"
+          className="mt-4 btn btn-primary btn-block capitalize"
+        >
+          Register
         </button>
         <button
           onClick={registerWithGoogle}
-          className="btn btn-primary w-full  mb-5"
+          className="btn btn-primary w-full mb-5"
         >
           <span className="text-2xl">Google</span>
         </button>
         <p className="text-center text-xl">
-          Not a member yet ?
+          Already a member?{" "}
           <Link to="/login" className="capitalize link">
             Login
           </Link>
